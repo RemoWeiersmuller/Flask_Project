2a74c90135506c2751ccd875a23dc1356691985c
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-11-24 11:54:05 +1000

    Complete the first tasks from Flask Project from prac 10

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+@app.route('/f')
+def f(fahrenheit: float):
+    celsius = 5/9*(float(fahrenheit)-32)
+    return f"{fahrenheit} is {celsius}"
 
 
 if __name__ == '__main__':
